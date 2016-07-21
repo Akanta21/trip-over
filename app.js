0706161c851002c407bb289fab4035147322de6d
Si Wai Joanne <ngsiwai@gmail.com>
2016-07-21 10:34:31 +0800

    add custom headers

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cityController = require('./controllers/city_controller')
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, User-Email, Auth-Token')
   next()
 })
 
